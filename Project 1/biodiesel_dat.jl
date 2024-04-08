c8944e0a7d755ac103974488b3d3806639d52219
niols <niolsson@gmail.com>
2024-04-08 14:43:41 +0200

    Random work

--- Project 1/biodiesel_dat.jl
+++ Project 1/biodiesel_dat.jl
@@ -7,8 +7,16 @@ fuels = ["B5", "B30", "B100"] # for i in I
 crops  = ["soybeans", "sunflower seeds", "cotton seeds"]
 
 #Parameters
-diesel_content = [0.95, 0.7, 0.0] 
-profit = [133/750, 0.4255, 62/75] #Profit per liter of fuel
-water_requirement = [5.0, 4.2, 1.0] #Ml per ha
+diesel_content = [0.95, 0.7, 0] 
+tax = [0.2, 0.05, 0]
+diesel_price = 1
+biodiesel_price = 1/3
+cost = diesel_content.*diesel_price.+([1,1,1].-diesel_content).*biodiesel_price
+price = [1.43, 1.29, 1.16]
+profit = price.*([1,1,1].-tax).-cost
+rain = 0
+water_requirement = [5.0, 4.2, 1.0] .- rain #Ml per ha
 yield = [2.6, 1.4, 0.9] #ton per ha
 oil_content = [0.178, 0.216, 0.433] #l per kg
+
+print(profit)
