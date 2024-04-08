c5bb2e7f015f6411f6480c13772aa55006f35f58
samuelollila <117409496+samuelollila@users.noreply.github.com>
2024-04-08 08:53:20 +0200

    Error fix

--- Project 1/biodiesel_dat.jl
+++ Project 1/biodiesel_dat.jl
@@ -10,5 +10,5 @@ crops  = ["soybeans", "sunflower seeds", "cotton seeds"]
 diesel_content = [0.95, 0.7, 0.0] 
 profit = [133/750, 0.4255, 62/75] #Profit per liter of fuel
 water_requirement = [5.0, 4.2, 1.0] #Ml per ha
-yield = [2.6, 1.4, 0.9] #ton per ha
+yieldd = [2.6, 1.4, 0.9] #ton per ha
 oil_content = [0.178, 0.216, 0.433] #l per kg
